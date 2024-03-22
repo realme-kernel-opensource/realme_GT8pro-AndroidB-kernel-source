f1e0c45a7318e4986bc40f9e75dbb2eadd6b61cf
Ravulapati Vishnu Vardhan Rao <quic_visr@quicinc.com>
2024-03-22 23:56:29 +0530

    wcd939x-i2c: add missing registers definition for v2.0
    Add missing register definitions for wcd939x version 2.0.
    
    Change-Id: I28b670d4478b92c7ac8ad309c6ce54dabed2f454
    Signed-off-by: Ravulapati Vishnu Vardhan Rao <quic_visr@quicinc.com>
    Signed-off-by: Sarath Varma Ganapathiraju <quic_ganavarm@quicinc.com>

--- drivers/soc/qcom/wcd-usbss-priv.h
+++ drivers/soc/qcom/wcd-usbss-priv.h
@@ -17,6 +17,7 @@
 #include <linux/usb/typec_mux.h>
 #include <linux/sched.h>
 #include <linux/soc/qcom/wcd939x-i2c.h>
+#include "wcd-usbss-registers.h"
 
 #define WCD_USBSS_SUPPLY_MAX 4
 
@@ -50,12 +51,11 @@ struct wcd_usbss_ctxt {
 	int wcd_standby_status;
 	int runtime_env_counter;
 	struct nvmem_cell *nvmem_cell;
-	bool defer_writes;
-	int req_state;
 	bool suspended;
 };
 
 extern struct regmap *wcd_usbss_regmap_init(struct device *dev,
 				   const struct regmap_config *config);
 extern struct regmap_config wcd_usbss_regmap_config;
+extern const u8 wcd_usbss_reg_access[WCD_USBSS_NUM_REGISTERS];
 #endif /* WCD_USBSS_PRIV_H */
