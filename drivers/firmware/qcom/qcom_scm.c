70c57b8b8242cb9c06821844dd24245f4a0764fc
Patrick Daly <quic_pdaly@quicinc.com>
2024-08-19 18:44:33 -0700

    firmware: qcom_scm: Avoid integer overflow in tempory variable
    Valid values of a vmid can be between 0 to 63. Define next_vm as
    a u64 to avoid overflow from below.
    
    next_vm |= BIT(newvm->vmid);
    
    Change-Id: I64fe7a8367316a0f65a98a77a33ae105131acefe
    Signed-off-by: Patrick Daly <quic_pdaly@quicinc.com>

--- drivers/firmware/qcom/qcom_scm.c
+++ drivers/firmware/qcom/qcom_scm.c
@@ -1317,7 +1317,7 @@ int qcom_scm_assign_mem(phys_addr_t mem_addr, size_t mem_sz,
 	size_t dest_sz;
 	size_t src_sz;
 	size_t ptr_sz;
-	int next_vm;
+	u64 next_vm;
 	__le32 *src;
 	int ret, i, b;
 	u64 srcvm_bits = *srcvm;
