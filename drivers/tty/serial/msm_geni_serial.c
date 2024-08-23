aosp-mirror/kernel_msm

serial: msm_geni_serial: skip buffer flush operation if there is no vote clock
Currently if vote clock is off and it tries to flush buffer and it
access QUPV3_0_SE5_GENI_STATUS register and it crashes.

To prevent crash, added additional checks that perform flush operation
only if we have active vote clock on.

Change-Id: I2ec4cf7ab282d3f25144a18b48275ae534e5343b
Signed-off-by: Mehul Raninga <quic_mraninga@quicinc.com>

drivers/tty/serial/msm_geni_serial.c
@@ -3508,8 +3508,10 @@ static void msm_geni_serial_flush(struct uart_port *uport)
 {
 	struct msm_geni_serial_port *port = GET_DEV_PORT(uport);
 
-	atomic_set(&port->flush_buffers, 1);
-	stop_tx_sequencer(uport);
+	if (port->ioctl_count) {
+		atomic_set(&port->flush_buffers, 1);
+		stop_tx_sequencer(uport);
+	}
 }
 
 static void msm_geni_serial_shutdown(struct uart_port *uport)

realme-kernel-opensource/realme_GT8pro-AndroidB-kernel-source

Signed-off-by: Chandana Kishori Chiluveru <quic_cchiluve@quicinc.com>

drivers/tty/serial/msm_geni_serial.c
@@ -3936,8 +3936,10 @@ static void msm_geni_serial_flush(struct uart_port *uport)
 {
 	struct msm_geni_serial_port *port = GET_DEV_PORT(uport);
 
-	atomic_set(&port->flush_buffers, 1);
-	msm_geni_serial_stop_tx(uport);
+	if (port->ioctl_count) {
+		atomic_set(&port->flush_buffers, 1);
+		msm_geni_serial_stop_tx(uport);
+	}
 }
 
 static void msm_geni_serial_shutdown(struct uart_port *uport)