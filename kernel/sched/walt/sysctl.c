6689b53cb6b7e64a8ded17dfabb0e840f86cb472
Ashay Jaiswal <quic_ashayj@quicinc.com>
2025-01-06 16:02:18 +0530

    sched: walt: add node to configure pipeline re-arrange
    Add sysfs node "sched_pipeline_rearrange_delay_ms" to configure the
    delay for re-evaluation of top 3 task for pipeline(Auto pipeline) and
    re-arranging of pipeline task between gold and prime.
    
    sysctl_pipeline_rearrange_delay_ms is a tuple for configuring:
    1. Delay between re-evaluation of top 3 tasks for pipeline.
    2. Delay between re-arranging of tasks between gold and prime.
    
    Change-Id: Ifc1e4666882f6ea1e00147b40cfafb1b4bafceb4
    Signed-off-by: Ashay Jaiswal <quic_ashayj@quicinc.com>

--- kernel/sched/walt/sysctl.c
+++ kernel/sched/walt/sysctl.c
@@ -130,6 +130,7 @@ unsigned int sysctl_pipeline_special_task_util_thres;
 unsigned int sysctl_pipeline_non_special_task_util_thres;
 unsigned int sysctl_pipeline_pin_thres_low_pct;
 unsigned int sysctl_pipeline_pin_thres_high_pct;
+unsigned int sysctl_pipeline_rearrange_delay_ms[2] = {100, 0};
 
 /* range is [1 .. INT_MAX] */
 static int sysctl_task_read_pid = 1;
@@ -1924,6 +1925,20 @@ static struct ctl_table walt_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec_minmax,
 		.extra1		= SYSCTL_ZERO,
+		.extra2		= SYSCTL_INT_MAX,
+	},
+	{
+		/*
+		 * A tuple to configure following delay:
+		 * 1st val: delay between re-evaluation of pipeline tasks.
+		 * 2nd val: delay between re-arranging pipeline tasks between prime and gold.
+		 */
+		.procname	= "sched_pipeline_rearrange_delay_ms",
+		.data		= &sysctl_pipeline_rearrange_delay_ms,
+		.maxlen		= sizeof(int) * 2,
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec_minmax,
+		.extra1		= SYSCTL_ONE,
 		.extra2		= SYSCTL_INT_MAX,
 	},
 	{
