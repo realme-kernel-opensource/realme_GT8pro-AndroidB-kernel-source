85781575e92f447691cbbbf5b6bb50b936816f20
Sai Harshini Nimmala <quic_snimmala@quicinc.com>
2025-02-07 15:07:56 -0800

    sched/cpufreq_walt: Cleanup function
    The reasons member can be updated in the post_update_cleanups function
    which leads to waltgov_get_util being an unnecessary wrapper around
    cpu_util_freq_walt.
    Hence, remove it.
    
    Change-Id: Icf4ff34002deac35d29ad6d8d62967296880896f
    Signed-off-by: Sai Harshini Nimmala <quic_snimmala@quicinc.com>
    Signed-off-by: Ashay Jaiswal <quic_ashayj@quicinc.com>

--- kernel/sched/walt/cpufreq_walt.c
+++ kernel/sched/walt/cpufreq_walt.c
@@ -339,6 +339,7 @@ void post_update_cleanups(struct waltgov_policy *wg_policy)
 		wg_cpu->rtg_boost_flag = false;
 		wg_cpu->hispeed_flag = false;
 		wg_cpu->conservative_pl_flag = false;
+		wg_cpu->reasons = 0;
 	}
 
 	wg_policy->rtg_boost_flag = false;
@@ -541,12 +542,6 @@ out:
 	return clamp(util, min_util, max_util);
 }
 
-static unsigned long waltgov_get_util(struct waltgov_cpu *wg_cpu)
-{
-	wg_cpu->reasons = 0;
-	return cpu_util_freq_walt(wg_cpu->cpu, &wg_cpu->walt_load, &wg_cpu->reasons);
-}
-
 #define NL_RATIO 75
 #define DEFAULT_HISPEED_LOAD 90
 #define DEFAULT_SILVER_RTG_BOOST_FREQ 1000000
@@ -694,7 +689,7 @@ static void waltgov_update_freq(struct waltgov_callback *cb, u64 time,
 	if (!wg_policy->tunables->pl && flags & WALT_CPUFREQ_PL_BIT)
 		return;
 
-	wg_cpu->util = waltgov_get_util(wg_cpu);
+	wg_cpu->util = cpu_util_freq_walt(wg_cpu->cpu, &wg_cpu->walt_load, &wg_cpu->reasons);
 	wg_cpu->flags = flags;
 	raw_spin_lock(&wg_policy->update_lock);
 
