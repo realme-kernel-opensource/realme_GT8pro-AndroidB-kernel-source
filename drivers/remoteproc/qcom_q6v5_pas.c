d187217e414a217e952a0b98e8d6886da474b307
David Gantman <quic_dgantman@quicinc.com>
2025-02-11 16:33:25 -0800

    remoteproc: q6v5: Set running state in late attach
    Set subsystem's running state to true when attaching to subsytem.
    Previously the subsystem's running state was not set to true for early
    boot subsystems, causing functions like fatal interrupt handler to
    behave incorrectly for those remoteprocs.
    
    Change-Id: I4f5c84f0ee9e1cdf2df689e00aaf8e7aa4b00757
    Signed-off-by: David Gantman <quic_dgantman@quicinc.com>

--- drivers/remoteproc/qcom_q6v5_pas.c
+++ drivers/remoteproc/qcom_q6v5_pas.c
@@ -5,7 +5,7 @@
  * Copyright (C) 2016 Linaro Ltd
  * Copyright (C) 2014 Sony Mobile Communications AB
  * Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
- * Copyright (c) 2023-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2023-2025 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/clk.h>
@@ -1205,6 +1205,7 @@ static int adsp_attach(struct rproc *rproc)
 			rproc->state = RPROC_CRASHED;
 			panic("Panicking, timed out on ping/pong for %s\n", rproc->name);
 		}
+		adsp->q6v5.running = true;
 	}
 
 	return ret;
