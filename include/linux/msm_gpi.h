45a9e6c3c82fd69daafb6287a3963bb530a5de2e
Prasanna S <quic_prass@quicinc.com>
2025-02-21 13:56:17 +0530

    dmaengine: gpi: Add API to retrieve TRE size from GPII channel
    Introduce a helper function `get_gpii_chan_req_tres()` to allow
    client drivers to query the configured TRE size for a given DMA
    channel. This is useful for validating resource availability before
    initiating multi-descriptor transfers.
    
    The function is exported for use by other kernel modules.
    
    Change-Id: Ic55bfe2ef196ad83befa56c4e8eceee7e2edd418
    Signed-off-by: Prasanna S <quic_prass@quicinc.com>

--- include/linux/msm_gpi.h
+++ include/linux/msm_gpi.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Copyright (c) 2017-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
 #ifndef __MSM_GPI_H_
@@ -264,6 +264,7 @@ if (print) { \
 
 #define GSI_MAX_NUM_TRE_MSGS		(448)
 #define GSI_MAX_IMMEDIATE_DMA_LEN	(8)
+#define DEFAULT_TRE_SIZE		(64)
 
 #define QUP_DMA_ADDR_ALIGN_BYTE		(16)
 #define QUP_SPLIT_DMA_TRE_SIZE		(16)
@@ -425,6 +426,15 @@ struct gsi_common {
 	int *protocol_err; /* protocol specific error*/
 	void (*ev_cb_fun)(struct dma_chan *ch, struct msm_gpi_cb const *cb_str, void *ptr);
 };
+
+/**
+ * get_gpii_chan_req_tres() - Retrieve the TRE size (`req_tres`) for
+ *                            the specified GPII channel from the DTSI
+ * @chan: Base address of the DMA channel
+ *
+ * Return: The TRE size of the GPII channel.
+ */
+u32 get_gpii_chan_req_tres(struct dma_chan *chan);
 
 /* Client drivers of the GPI can call this function to dump the GPI registers
  * whenever client met some scenario like timeout, error in GPI transfer mode.
