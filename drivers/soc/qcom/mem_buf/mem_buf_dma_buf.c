af5d5ff96e471866433936d2e85570c8ebe76114
Patrick Daly <quic_pdaly@quicinc.com>
2025-02-26 17:56:50 -0800

    mem_buf: Add a debugfs file to show per-vm memory stats
    Display the amount of secure dma-buf memory in debugfs. If a memory
    region of size S is shared to N VMIDs, it will be accounted as S/N
    in the PSS-SIZE-MB column and S in the SIZE-MB column.
    
    > cat /sys/kernel/debug/mem_buf/mem_buf_summary
          VMID    SIZE-MB PSS-SIZE-MB
            2d         64         32
            31         64         32
             9          0          0
             a          0          0
             b          0          0
    
    Change-Id: I6115b85ef12445f45b8c6a71e416f14755331dcb
    Signed-off-by: Patrick Daly <quic_pdaly@quicinc.com>

--- drivers/soc/qcom/mem_buf/mem_buf_dma_buf.c
+++ drivers/soc/qcom/mem_buf/mem_buf_dma_buf.c
@@ -9,6 +9,7 @@
 #include <linux/highmem.h>
 #include <linux/mem-buf-exporter.h>
 #include <linux/gunyah/gh_mem_notifier.h>
+#include <linux/debugfs.h>
 #include "mem-buf-dev.h"
 #include "mem-buf-gh.h"
 #include "mem-buf-ids.h"
@@ -29,6 +30,7 @@ struct mem_buf_vmperm {
 	unsigned int max_acl_entries;
 	struct dma_buf *dmabuf;
 	struct sg_table *sgt;
+	size_t size;
 	gh_memparcel_handle_t memparcel_hdl;
 	struct mutex lock;
 	mem_buf_dma_buf_destructor dtor;
@@ -133,7 +135,8 @@ static struct mem_buf_vmperm *mem_buf_vmperm_alloc_flags(
 	gh_memparcel_handle_t hdl)
 {
 	struct mem_buf_vmperm *vmperm;
-	int ret;
+	struct scatterlist *sg;
+	int ret, i;
 
 	vmperm = kzalloc(sizeof(*vmperm), GFP_KERNEL);
 	if (!vmperm)
@@ -150,6 +153,9 @@ static struct mem_buf_vmperm *mem_buf_vmperm_alloc_flags(
 					nr_acl_entries);
 	mutex_unlock(&vmperm->lock);
 	vmperm->sgt = sgt;
+	for_each_sgtable_sg(sgt, sg, i)
+		vmperm->size += sg->length;
+
 	vmperm->flags = flags;
 	vmperm->memparcel_hdl = hdl;
 	vmperm->kref_release = release;
@@ -886,6 +892,64 @@ static void mem_buf_vmperm_gh_notifier(enum gh_mem_notifier_tag tag, unsigned lo
 	kref_put(vmperm->kref, vmperm->kref_release);
 }
 
+struct summary_data {
+	int vmid;
+	size_t total;
+	size_t total_pss;
+};
+
+#define TO_MB(x) (x >> 20)
+
+static int summary_show(struct seq_file *s, void *unused)
+{
+	struct summary_data d[] = {
+		{.vmid = VMID_TVM},
+		{.vmid = VMID_OEMVM},
+		{.vmid = VMID_CP_BITSTREAM},
+		{.vmid = VMID_CP_PIXEL},
+		{.vmid = VMID_CP_NON_PIXEL},
+	};
+	struct mem_buf_vmperm *vmperm;
+	size_t total_pss = 0;
+	int i, j;
+
+	mutex_lock(&vmperm_list_lock);
+	list_for_each_entry(vmperm, &vmperm_list, list) {
+		mutex_lock(&vmperm->lock);
+		for (i = 0; i < vmperm->nr_acl_entries; i++) {
+			for (j = 0; j < ARRAY_SIZE(d); j++) {
+				if (vmperm->vmids[i] == d[j].vmid) {
+					d[j].total += vmperm->size;
+					d[j].total_pss += vmperm->size / vmperm->nr_acl_entries;
+					total_pss += d[j].total_pss;
+				}
+			}
+		}
+		mutex_unlock(&vmperm->lock);
+	}
+	mutex_unlock(&vmperm_list_lock);
+
+	seq_printf(s, "%10s %10s %10s\n", "VMID", "SIZE-MB", "PSS-SIZE-MB");
+	for (j = 0; j < ARRAY_SIZE(d); j++)
+		seq_printf(s, "%#10x %10zu %10zu\n", d[j].vmid, TO_MB(d[j].total),
+			   TO_MB(d[j].total_pss));
+	seq_printf(s, "\nTotal Pss: %zu\n", TO_MB(total_pss));
+
+	return 0;
+}
+
+static int summary_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, summary_show, NULL);
+}
+
+static const struct file_operations summary_fops = {
+	.open		= summary_open,
+	.read		= seq_read,
+	.llseek		= seq_lseek,
+	.release	= seq_release,
+};
+
 int mem_buf_dma_buf_init(void)
 {
 	gh_rm_mem_notifier_cookie = gh_mem_notifier_register(GH_MEM_NOTIFIER_TAG_MEM_BUF,
@@ -895,5 +959,7 @@ int mem_buf_dma_buf_init(void)
 		return PTR_ERR(gh_rm_mem_notifier_cookie);
 	}
 
+	debugfs_create_file("mem_buf_summary", 0400, mem_buf_debugfs_root, NULL,
+			    &summary_fops);
 	return 0;
 }
