cd72d98c4e43aa63c3f7dba7761b9660997da3e5
Vivek Aknurwar <quic_viveka@quicinc.com>
2025-02-28 13:09:09 -0800

    clk: qcom: dispcc-canoe: Remove HW_CTRL flag from mdss core GDSC
    Remove HW_CTRL flag from mdss core GDSC for below reasons,
    1. mdss core GDSC is kept on in SW_CTRL from boot; if dispcc probe
    tries to switch to HW_CTRL and if there are no votes to display CESTA
    then this switch will result in temporary turning off GDSC. This breaks
    Display splash use case.
    2. HW_CTRL switch can be done in probe but it further complicates init
    sequence dependencies wrt dispcc, rsc and crm driver.
    3. HW_CTRL flag as in general can be set by consumers than set in clock
    controller registration.
    
    Change-Id: I5405085a39418fc88acbd56031afb01cb0fda499
    Signed-off-by: Vivek Aknurwar <quic_viveka@quicinc.com>

--- drivers/clk/qcom/dispcc-canoe.c
+++ drivers/clk/qcom/dispcc-canoe.c
@@ -2138,7 +2138,7 @@ static struct gdsc disp_cc_mdss_core_gdsc = {
 		.name = "disp_cc_mdss_core_gdsc",
 	},
 	.pwrsts = PWRSTS_OFF_ON,
-	.flags = POLL_CFG_GDSCR | RETAIN_FF_ENABLE | HW_CTRL_SKIP_DIS | HW_CTRL_TRIGGER | HW_CTRL,
+	.flags = POLL_CFG_GDSCR | RETAIN_FF_ENABLE | HW_CTRL_SKIP_DIS | HW_CTRL_TRIGGER,
 	.supply = "vdd_mm",
 };
 
