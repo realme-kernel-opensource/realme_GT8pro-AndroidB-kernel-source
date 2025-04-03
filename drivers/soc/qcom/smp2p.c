c1dc950c83c2b60544285961ef00153f7812c296
Mohit Sharma <quic_smohit@quicinc.com>
2025-04-03 03:51:00 -0700

    Revert "soc: qcom: smp2p: Mirror remote version"
    This reverts commit d02fbdd7ba32c5c538e7a750d157efe895e9f622.
    
    Change-Id: I74be8d36e2148766aa0506d421620eeb88109e98
    Signed-off-by: Mohit Sharma <quic_smohit@quicinc.com>

--- drivers/soc/qcom/smp2p.c
+++ drivers/soc/qcom/smp2p.c
@@ -2,7 +2,7 @@
 /*
  * Copyright (c) 2015, Sony Mobile Communications AB.
  * Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2025, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2024, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/interrupt.h>
@@ -220,7 +220,6 @@ static void qcom_smp2p_negotiate(struct qcom_smp2p *smp2p)
 	struct smp2p_smem_item *out = smp2p->out;
 	struct smp2p_smem_item *in = smp2p->in;
 
-	SMP2P_INFO("%d: version[%d:%d]\n", smp2p->remote_pid, in->version, out->version);
 	if (in->version == out->version) {
 		out->features &= in->features;
 
@@ -231,20 +230,6 @@ static void qcom_smp2p_negotiate(struct qcom_smp2p *smp2p)
 		SMP2P_INFO("%d: state=open ssr_ack=%d\n", smp2p->remote_pid,
 			   smp2p->ssr_ack_enabled);
 	}
-}
-
-static int qcom_smp2p_in_version(struct qcom_smp2p *smp2p)
-{
-	unsigned int smem_id = smp2p->smem_items[SMP2P_INBOUND];
-	unsigned int pid = smp2p->remote_pid;
-	struct smp2p_smem_item *in;
-	size_t size;
-
-	in = qcom_smem_get(pid, smem_id, &size);
-	if (IS_ERR(in))
-		return 0;
-
-	return in->version;
 }
 
 static void qcom_smp2p_start_in(struct qcom_smp2p *smp2p)
@@ -569,7 +554,6 @@ static int qcom_smp2p_alloc_outbound_item(struct qcom_smp2p *smp2p)
 	struct smp2p_smem_item *out;
 	unsigned smem_id = smp2p->smem_items[SMP2P_OUTBOUND];
 	unsigned pid = smp2p->remote_pid;
-	u8 in_version;
 	int ret;
 
 	ret = qcom_smem_alloc(pid, smem_id, sizeof(*out));
@@ -594,14 +578,12 @@ static int qcom_smp2p_alloc_outbound_item(struct qcom_smp2p *smp2p)
 	out->valid_entries = 0;
 	out->features = SMP2P_ALL_FEATURES;
 
-	in_version = qcom_smp2p_in_version(smp2p);
-
 	/*
 	 * Make sure the rest of the header is written before we validate the
 	 * item by writing a valid version number.
 	 */
 	wmb();
-	out->version = (in_version) ? in_version : 1;
+	out->version = 1;
 
 	qcom_smp2p_kick(smp2p);
 
