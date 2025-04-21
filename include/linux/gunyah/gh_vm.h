eae15e6546aedbd1f48bbe916aed2e3b5b1d2ff0
Prakruthi Deepak Heragu <quic_pheragu@quicinc.com>
2025-04-21 12:45:07 -0700

    gunyah: gh_vm: Register with upstream notifier
    If QTVM is being launched by the ACK driver, then clients will listen
    to the VM life cycle notifications from the ACK driver.
    
    Change-Id: I05be4ac02051facedc5e81b9aa33354ff16327e3
    Signed-off-by: Prakruthi Deepak Heragu <quic_pheragu@quicinc.com>

--- include/linux/gunyah/gh_vm.h
+++ include/linux/gunyah/gh_vm.h
@@ -6,7 +6,17 @@
 #ifndef _GH_VM_H
 #define _GH_VM_H
 
-/* VM power notifications */
+#include <linux/gunyah_qtvm.h>
+
+#ifdef CONFIG_QTVM_WITH_AVF
+#define GH_VM_BEFORE_POWERUP		GUNYAH_QTVM_BEFORE_POWERUP
+#define GH_VM_POWERUP_FAIL		GUNYAH_QTVM_POWERUP_FAIL
+#define GH_VM_EARLY_POWEROFF		GUNYAH_QTVM_EARLY_POWEROFF
+#define GH_VM_POWEROFF			GUNYAH_QTVM_POWEROFF
+#define GH_VM_EXITED			GUNYAH_QTVM_EXITED
+#define GH_VM_CRASH			GUNYAH_QTVM_CRASH
+
+#else
 #define GH_VM_BEFORE_POWERUP		0x1
 #define GH_VM_POWERUP_FAIL		0x2
 #define GH_VM_EARLY_POWEROFF		0x3
@@ -14,9 +24,24 @@
 #define GH_VM_EXITED			0x5
 #define GH_VM_CRASH			0x6
 
-#if IS_ENABLED(CONFIG_GH_SECURE_VM_LOADER)
+#endif
+
+#ifdef CONFIG_QTVM_WITH_AVF
+/* VM power notifications */
+static inline int gh_register_vm_notifier(struct notifier_block *nb)
+{
+	return gunyah_qtvm_register_notifier(nb);
+}
+
+static inline int gh_unregister_vm_notifier(struct notifier_block *nb)
+{
+	return gunyah_qtvm_unregister_notifier(nb);
+}
+
+#elif IS_ENABLED(CONFIG_GH_SECURE_VM_LOADER)
 int gh_register_vm_notifier(struct notifier_block *nb);
 int gh_unregister_vm_notifier(struct notifier_block *nb);
+
 #else
 static inline int gh_register_vm_notifier(struct notifier_block *nb)
 {
