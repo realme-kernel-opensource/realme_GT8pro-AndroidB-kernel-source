686a4c479748ee9c2988a83c1a2b5aa8437f6493
Jagadeesh Kona <quic_jkona@quicinc.com>
2025-06-03 10:44:31 +0530

    clk: qcom: videocc-canoe: Add 800MHz support for videocc mvs0b clk src
    Update the max frequency support from 710MHz to 800MHz for videocc
    mvs0b clk src on canoe v2 platform.
    
    Change-Id: I4505210b66fbdbf6f97491ab5f9f39de064fbab2
    Signed-off-by: Jagadeesh Kona <quic_jkona@quicinc.com>

--- drivers/clk/qcom/videocc-canoe.c
+++ drivers/clk/qcom/videocc-canoe.c
@@ -388,7 +388,7 @@ static const struct freq_tbl ftbl_video_cc_mvs0b_clk_src_canoe_v2[] = {
 	F(444000000, P_VIDEO_CC_PLL2_OUT_MAIN, 2, 0, 0),
 	F(533000000, P_VIDEO_CC_PLL2_OUT_MAIN, 2, 0, 0),
 	F(630000000, P_VIDEO_CC_PLL2_OUT_MAIN, 2, 0, 0),
-	F(710000000, P_VIDEO_CC_PLL2_OUT_MAIN, 2, 0, 0),
+	F(800000000, P_VIDEO_CC_PLL2_OUT_MAIN, 2, 0, 0),
 	{ }
 };
 
@@ -935,7 +935,7 @@ static int video_cc_canoe_fixup(struct platform_device *pdev, struct regmap *reg
 		video_cc_mvs0b_clk_src.freq_tbl = ftbl_video_cc_mvs0b_clk_src_canoe_v2;
 		video_cc_mvs0_clk_src.clkr.vdd_data.rate_max[VDD_HIGH_L0] = 800000000;
 		video_cc_mvs0_clk_src.clkr.vdd_data.rate_max[VDD_HIGH_L1] = 1000000000;
-		video_cc_mvs0b_clk_src.clkr.vdd_data.rate_max[VDD_HIGH_L1] = 710000000;
+		video_cc_mvs0b_clk_src.clkr.vdd_data.rate_max[VDD_HIGH_L1] = 800000000;
 		video_cc_mvs0c_clk_src.clkr.vdd_data.rate_max[VDD_HIGH_L0] = 1260000000;
 	}
 
