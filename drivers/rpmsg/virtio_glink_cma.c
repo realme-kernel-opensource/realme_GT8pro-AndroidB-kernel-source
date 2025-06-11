Tokito-to/kernel_xiaomi_peridot

rpmsg: glink_cma: Update cdsp and adsp label list for Nords
Update cdsp2-3 and adsp0-2 label list which will be matched with PVM's
label data.

Change-Id: Ieb1ac064eaa81fb15d28799456c4d77aaa6e11ec
Signed-off-by: Meenu Raja Sundaram <quic_mrajasun@quicinc.com>

drivers/rpmsg/virtio_glink_cma.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2023-2024, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2023-2025, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/delay.h>
@@ -38,6 +38,11 @@
 enum {
 	CDSP0,
 	CDSP1,
+	CDSP2,
+	CDSP3,
+	ADSP0,
+	ADSP1,
+	ADSP2,
 	DSP_MAX,
 	DSP_ERR = 0xff
 };
@@ -45,6 +50,11 @@ enum {
 static const char * const to_dsp_str[DSP_MAX] = {
 	[CDSP0] = "cdsp",
 	[CDSP1] = "cdsp1",
+	[CDSP2] = "cdsp2",
+	[CDSP3] = "cdsp3",
+	[ADSP0] = "adsp0",
+	[ADSP1] = "adsp1",
+	[ADSP2] = "adsp2",
 };
 
 #define DSP_LABEL_TO_STR(dsp) (((dsp) >= DSP_MAX) ? "INVALID DSP" : to_dsp_str[(dsp)])

realme-kernel-opensource/realme_GT8pro-AndroidB-kernel-source

drivers/rpmsg/virtio_glink_cma.c
@@ -38,6 +38,11 @@
 enum {
 	CDSP0,
 	CDSP1,
+	CDSP2,
+	CDSP3,
+	ADSP0,
+	ADSP1,
+	ADSP2,
 	DSP_MAX,
 	DSP_ERR = 0xff
 };
@@ -45,6 +50,11 @@ enum {
 static const char * const to_dsp_str[DSP_MAX] = {
 	[CDSP0] = "cdsp",
 	[CDSP1] = "cdsp1",
+	[CDSP2] = "cdsp2",
+	[CDSP3] = "cdsp3",
+	[ADSP0] = "adsp",
+	[ADSP1] = "adsp1",
+	[ADSP2] = "adsp2",
 };
 
 #define DSP_LABEL_TO_STR(dsp) (((dsp) >= DSP_MAX) ? "INVALID DSP" : to_dsp_str[(dsp)])