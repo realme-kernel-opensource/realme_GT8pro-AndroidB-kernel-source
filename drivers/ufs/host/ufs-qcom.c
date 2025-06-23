7d4bf66340aa89a2fc278dfaa8c10ec71427093c
Ziqi Chen <quic_ziqichen@quicinc.com>
2025-06-23 15:56:35 +0800

    scsi: ufs: qcom: corrected LF copyright year
    When we migrated from codeaurora to CLO by end of 2021, shouldn't update
    it to 2022, corrected it back to 2021.
    
    Change-Id: I6b62b593d9f22ad97c73dc27332d69e511927ff1
    Signed-off-by: Ziqi Chen <quic_ziqichen@quicinc.com>

--- drivers/ufs/host/ufs-qcom.c
+++ drivers/ufs/host/ufs-qcom.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2013-2022, Linux Foundation. All rights reserved.
+ * Copyright (c) 2013-2021, Linux Foundation. All rights reserved.
  * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
